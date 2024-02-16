6b2e39db5ae89c270d89b8faa8782c79bbf23bda
pramataroff <milenpramatarov@gmail.com>
2024-02-16 12:33:57 +0200

    third try

---
+++ zadacha 16.02/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System.Threading.Channels;
+
+namespace zadacha_16._02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+             if (numbers.Length == 1) 
+            {
+                Console.WriteLine(numbers[0]);
+            }
+
+            else if (numbers.Length % 2 == 0 ) 
+            {
+                Console.WriteLine(numbers.Length / 2);
+                Console.WriteLine(numbers.Length / 2 + 1);
+            }
+            else if (numbers.Length % 2 == 1 )
+            {
+                Console.WriteLine(numbers.Length/2);
+                Console.WriteLine(numbers.Length/2 + 1);
+                Console.WriteLine(numbers.Length / 2 + 2);
+            }
+            
+        }
+    }
+}
